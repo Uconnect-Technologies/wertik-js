5bec41d5d76cbd5c39df2461aeea71ca15b619cb
ilyaskarim <iksdatoo@gmail.com>
2019-08-22 13:03:02 +0500

    Removed content

--- src/framework/predefinedModules/forgetPassword/mutation.ts
+++ src/framework/predefinedModules/forgetPassword/mutation.ts
@@ -1,8 +1,3 @@
-const {dialect} = process.env;
-let relationSchemaType = "Int";
-if (dialect == "MONGO_DB") {
-	relationSchemaType = "String";
-}
 export default `
   requestPasswordReset(input: ForgetPasswordInput): ForgetPassword
   resetPassword(input: ForgetPasswordInput): ForgetPassword
