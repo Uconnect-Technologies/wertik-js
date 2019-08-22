060809e01ac92759726f3ba6ea62ab0061fdae65
ilyaskarim <iksdatoo@gmail.com>
2019-08-22 13:27:48 +0500

    Added lib folder to gitignore, renamed file throughError.ts to throwError.ts, lol it was wrong renamed

--- src/framework/helpers/throwError.ts
+++ src/framework/helpers/throwError.ts
@@ -1,4 +1,4 @@
-export default function() {
+export default function(message, errorCode) {
   // for = "apollo-graphql", errorCode = 500, error
   // if (for == "apollo-graphql") {
   // }else if (for == "rest-api") {
--- src/framework/predefinedModules/forgetPassword/mutation.ts
+++ src/framework/predefinedModules/forgetPassword/mutation.ts
@@ -1,3 +1,5 @@
+let { ApolloError } = require("apollo-server");
+
 export default `
   requestPasswordReset(input: ForgetPasswordInput): ForgetPassword
   resetPassword(input: ForgetPasswordInput): ForgetPassword
