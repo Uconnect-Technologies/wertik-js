ce49a9eb682292fc9d6bb188aa409ba13cca8da4
ilyaskarim <ilyas.datoo@gmail.com>
2019-09-20 00:18:56 +0500

    Changes in app

--- src/framework/helpers/index.ts
+++ src/framework/helpers/index.ts
@@ -1,4 +1,21 @@
 let {ApolloError} = require("apollo-server");
+import fs from "fs";
+
 export const generateError = (e: any,statusCode: Number = 404) => {
     return new ApolloError(e.message);
+}
+
+export const getDirectoriesInFolder = (path: string) => {
+    return fs.readdirSync(path).filter(function (file: any) {
+		return fs.statSync(path+'/'+file).isDirectory();
+	});
+}
+
+export const exists = (path: any) => {
+	try{
+		fs.accessSync(path);
+	} catch (err){
+		return false;
+	}
+	return true;
 }
