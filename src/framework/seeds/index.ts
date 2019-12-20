9c4bd83194dc700068590262932655fa69432a9a
waseem <waseembarcha1993@gmail.com>
2019-12-21 04:49:41 +0500

    Changes

--- src/framework/seeds/index.ts
+++ src/framework/seeds/index.ts
@@ -1,23 +1,29 @@
 export default function(configuration, models) {
-  return function() {
-    const seeds = configuration.seeds;
-    const modules = Object.keys(seeds);
+  return function(modules: Array<string>) {
     return new Promise((resolve, reject) => {
-      if (modules.length == 0) {
-        resolve("No Seeds provided.");
-        return;
-      }
-      modules.forEach((currentModule, index) => {
-        let isEnd = modules.length - 1 == index;
-        let moduleData = seeds[currentModule];
-        let model = models[currentModule];
-        moduleData.forEach((element, indexOfData) => {
-          model.create(element);
-          if (isEnd && indexOfData == moduleData.length - 1) {
-            resolve("Seeds added");
+      if (modules && modules.constructor == Array) {
+        const seeds = configuration.seeds;
+          if (modules.length == 0) {
+            resolve("No Seeds provided.");
+            return;
           }
-        });
-      });
+          modules.forEach((currentModule, index) => {
+            let isEnd = modules.length - 1 == index;
+            let moduleData = seeds[currentModule];
+            let model = models[currentModule];
+            moduleData.forEach((element, indexOfData) => {
+              model.create(element);
+              if (isEnd && indexOfData == moduleData.length - 1) {
+                resolve(`Seeds added for modules: ${modules.join(", ")}`);
+              }
+            });
+          });
+        
+      }else {
+        let message = `[Wertik Seeds]: modules expected in this format: Array<string>, but received: ${modules}`;
+        console.error(message);
+        reject(message);
+      }
     });
   };
 }
