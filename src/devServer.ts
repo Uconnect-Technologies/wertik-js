61a5e8a0534bf0bae5e26e63fe52a97097d21672
ilyas <ilyas.datoo@gmail.com>
2020-04-02 21:11:05 +0500

    fixes #160

--- src/devServer.ts
+++ src/devServer.ts
@@ -4,9 +4,8 @@ import { IConfiguration } from "./framework/types/configuration";
 const defaultConfiguration: IConfiguration = require("./framework/defaults/defaultConfigurations/defaultConfiguration").default;
 const postgresConfiguration: IConfiguration = require("./framework/defaults/defaultConfigurations/postgresConfiguration").default;
 
-
 let app = express();
 
-wertik({ expressApp: app }, defaultConfiguration).then((p: any) => {
-  p.database.sync();
+wertik({ expressApp: app }, defaultConfiguration).then((wertikApp: any) => {
+  wertikApp.database.sync();
 });
--- src/framework/database/loadTables.ts
+++ src/framework/database/loadTables.ts
@@ -57,6 +57,5 @@ export default function(connection, configuration) {
     }
     processModule(module);
   });
-  connection.sync();
   return tables;
 }
