1c83cb61eb1814621e5d31019c3fda6ed71e57f4
ilyas <ilyas.datoo@gmail.com>
2020-04-28 10:39:04 +0500

    Connecting to mongo db

--- src/framework/database/connect.ts
+++ src/framework/database/connect.ts
@@ -2,59 +2,70 @@ import { successMessage } from "../logger/consoleMessages";
 import { IConfiguration } from "../types/configuration";
 import { get } from "lodash";
 
-let Sequelize = require("sequelize");
-export default function (configurationObject: IConfiguration) {
-  let DB_PRODUCTION;
-  let dialect = configurationObject.database.dbDialect;
-  let database = configurationObject.database;
-  let options;
-  if (dialect == "postgres") {
-    options = get(configurationObject, "database.dbInitializeOptions", {
-      logging: false,
-      operatorsAliases: false,
-      dialectOptions: {
-        ssl: true,
-      },
-    });
-    DB_PRODUCTION = new Sequelize(`${database.dbName}`, database.dbUsername, database.dbPassword, {
-      dialect: "postgres",
-      host: database.dbHost,
-      port: database.dbPort,
-      ...options,
-    });
-  } else if (dialect == "mongodb") {
-    // Steps to make mongo DB complete
+let Sequelize;
 
-    // 1. Create initializer. [In Progrss] [connect.ts]
-    // 2. Import modules and setup mongodb models from them [To do] [loadTables]
-    // 3. Create wertik model.ts function [To do] [src/framework/model/model.ts]
+export default async function (configurationObject: IConfiguration) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let DB_PRODUCTION;
+      let dialect = configurationObject.database.dbDialect;
+      if (dialect == "mysql" || dialect == "postgres") {
+        Sequelize = require("sequelize");
+      }
+      let database = configurationObject.database;
+      let options;
+      if (dialect == "postgres") {
+        options = get(configurationObject, "database.dbInitializeOptions", {
+          logging: false,
+          operatorsAliases: false,
+          dialectOptions: {
+            ssl: true,
+          },
+        });
+        DB_PRODUCTION = new Sequelize(`${database.dbName}`, database.dbUsername, database.dbPassword, {
+          dialect: "postgres",
+          host: database.dbHost,
+          port: database.dbPort,
+          ...options,
+        });
+      } else if (dialect == "mongodb") {
+        // Steps to make mongo DB complete
 
-    const mongoose = require("mongoose");
-    mongoose.connect(database.mongoDBURI, { useNewUrlParser: true, useUnifiedTopology: true });
-  } else {
-    options = get(configurationObject, "database.dbInitializeOptions", {
-      logging: false,
-      operatorsAliases: false,
-      underscored: false,
-      freetableName: true,
-    });
-    DB_PRODUCTION = new Sequelize(`${database.dbName}`, database.dbUsername, database.dbPassword, {
-      dialect: "mysql",
-      host: database.dbHost,
-      port: database.dbPort,
-      ...options,
-    });
-  }
-  if (dialect === "mysql" || dialect == "postgres") {
-    DB_PRODUCTION.authenticate()
-      .then(() => {
-        successMessage(`Database Connected`);
-      })
-      .catch((e) => {
-        console.log(e);
-        process.exit();
-      });
-  }
+        // 1. Create initializer. [In Progrss] [connect.ts]
+        // 2. Import modules and setup mongodb models from them [To do] [loadTables]
+        // 3. Create wertik model.ts function [To do] [src/framework/model/model.ts]
 
-  return DB_PRODUCTION;
+        const mongoose = require("mongoose");
+        await mongoose.connect(database.mongoDBURI, { useNewUrlParser: true, useUnifiedTopology: true });
+        
+      } else {
+        options = get(configurationObject, "database.dbInitializeOptions", {
+          logging: false,
+          operatorsAliases: false,
+          underscored: false,
+          freetableName: true,
+        });
+        DB_PRODUCTION = new Sequelize(`${database.dbName}`, database.dbUsername, database.dbPassword, {
+          dialect: "mysql",
+          host: database.dbHost,
+          port: database.dbPort,
+          ...options,
+        });
+      }
+      if (dialect === "mysql" || dialect == "postgres") {
+        DB_PRODUCTION.authenticate()
+          .then(() => {
+            successMessage(`Database Connected`);
+          })
+          .catch((e) => {
+            console.log(e);
+            process.exit();
+          });
+      }
+
+      resolve(DB_PRODUCTION);
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
