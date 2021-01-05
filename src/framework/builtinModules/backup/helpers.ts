63621c6e855d7191244bbd9c83def81a6ec33b62
Ilyas Karim <42450390+ilyaskarim@users.noreply.github.com>
2021-01-06 01:45:19 +0500

    Fix small issues and error reporting on backup

--- src/framework/builtinModules/backup/helpers.ts
+++ src/framework/builtinModules/backup/helpers.ts
@@ -2,105 +2,122 @@ import path from "path";
 import moment from "moment";
 import mysqldump from "mysqldump";
 import * as aws from "aws-sdk";
-import fs from "fs"
-import { Dropbox, Error, files } from 'dropbox'; // eslint-disable-line no-unused-vars
-
+import { get } from "lodash";
+import fs from "fs";
+import { Dropbox, Error, files } from "dropbox"; // eslint-disable-line no-unused-vars
 
 export const dumpDatabase = async ({ database, models }) => {
-  const filename = `backups/${moment().format("MMMM-DD-YYYY-h-mm-ss-a")}-database-${database.dbName}.sql.gz`;
-  
-  const backupInstance = await models.Backup.create({
-    uploaded_filename: filename,
-    uploaded_to: "local",
-  });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const filename = `backups/${moment().format("MMMM-DD-YYYY-h-mm-ss-a")}-database-${database.dbName}.sql`;
 
-  await mysqldump({
-    connection: {
-      host: database.dbHost,
-      user: database.dbUsername,
-      password: database.dbPassword,
-      database: database.dbName,
-    },
-    dumpToFile: filename,
-    compressFile: true,
-  });
+      const backupInstance = await models.Backup.create({
+        uploaded_filename: filename,
+        uploaded_to: "local",
+      });
 
-  return {
-    filename,
-    backupInstance,
-  };
+      setTimeout(async () => {
+        await mysqldump({
+          connection: {
+            host: database.dbHost,
+            user: database.dbUsername,
+            password: database.dbPassword,
+            database: database.dbName,
+          },
+          dumpToFile: filename,
+          // compressFile: true,
+        });
+
+        resolve({
+          filename,
+          backupInstance,
+        });
+      }, 1500);
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 export const dumpDatabaseToDigitalOcean = async ({ localDump, configuration }) => {
   try {
     const filename = localDump.filename;
-    const digitalOceanSpacesDetails = configuration.backup.digitalOceanSpaces;
+    const digitalOceanSpacesDetails = get(configuration, "backup.digitalOceanSpaces", null);
 
-  aws.config.update({
-    accessKeyId: digitalOceanSpacesDetails.accessKeyId,
-    secretAccessKey: digitalOceanSpacesDetails.secretAccessKey,
-  });
+    if (
+      digitalOceanSpacesDetails &&
+      digitalOceanSpacesDetails.accessKeyId &&
+      digitalOceanSpacesDetails.secretAccessKey &&
+      digitalOceanSpacesDetails.spacesEndpoint &&
+      digitalOceanSpacesDetails.uploadParams
+    ) {
+      aws.config.update({
+        accessKeyId: digitalOceanSpacesDetails.accessKeyId,
+        secretAccessKey: digitalOceanSpacesDetails.secretAccessKey,
+      });
 
-  const spacesEndpoint: any = new aws.Endpoint(digitalOceanSpacesDetails.spacesEndpoint);
+      const spacesEndpoint: any = new aws.Endpoint(digitalOceanSpacesDetails.spacesEndpoint);
 
-  const s3 = new aws.S3({
-    endpoint: spacesEndpoint,
-  });
+      const s3 = new aws.S3({
+        endpoint: spacesEndpoint,
+      });
 
-  const data = await fs.readFileSync(filename)
+      const data = await fs.readFileSync(filename);
 
-  const params = {
-    Bucket: digitalOceanSpacesDetails.uploadParams.Bucket,
-    Key: `${filename}`,
-    Body: data,
-    ACL: digitalOceanSpacesDetails.uploadParams.ACL,
-  };
-  
-  await s3.upload(params).promise();
+      const params = {
+        Bucket: digitalOceanSpacesDetails.uploadParams.Bucket,
+        Key: `${filename}`,
+        Body: data,
+        ACL: digitalOceanSpacesDetails.uploadParams.ACL,
+      };
 
-  localDump.backupInstance = await localDump.backupInstance.update({
-    uploaded_to: localDump.backupInstance.uploaded_to + ", digital_ocean",
-  });
+      await s3.upload(params).promise();
 
+      localDump.backupInstance = await localDump.backupInstance.update({
+        uploaded_to: localDump.backupInstance.uploaded_to + ", digital_ocean",
+      });
+    } else {
+      throw new Error("Please provide your digital ocean details to backup database.");
+    }
   } catch (e) {
-    throw new Error(`
-      Something went wrong while uploading backup to your digital ocean spaces object. \n
-      Message Received: ${e.message}
-    `);
+    throw new Error(e);
   }
 
   return localDump;
 };
 
-
 export const dumpDatabaseToDropbox = async ({ localDump, configuration }) => {
   try {
     const filename = localDump.filename;
-    const dropdboxDetails = configuration.backup.dropbox;
-    const dbx = new Dropbox({ accessToken: dropdboxDetails.accessToken });
+    const dropdboxDetails = get(configuration, "backup.dropbox", null);
 
-    const data: Buffer = await fs.readFileSync(filename);
-    
-    await dbx.filesUpload({ strict_conflict: false, path: `/${filename}`, contents: data });
+    if (dropdboxDetails && dropdboxDetails.accessToken) {
+      const dbx = new Dropbox({ accessToken: dropdboxDetails.accessToken });
 
-    localDump.backupInstance = await localDump.backupInstance.update({
-      uploaded_to: localDump.backupInstance.uploaded_to + ", dropbox",
-    });
+      const data: Buffer = await fs.readFileSync(filename);
 
-    return localDump;
-    
+      await dbx.filesUpload({ strict_conflict: false, path: `/${filename}`, contents: data });
+
+      localDump.backupInstance = await localDump.backupInstance.update({
+        uploaded_to: localDump.backupInstance.uploaded_to + ", dropbox",
+      });
+
+      return localDump;
+    } else {
+      throw new Error("Please provide your dropbox details to backup database.");
+    }
   } catch (e) {
-    console.log(e)
+    console.log(e);
     throw new Error(e.message);
   }
-}
+};
 
 export const loadAllLocalBackups = async () => {
   try {
-    const backupsPath = path.join(__dirname,'../../../../backups')
-  let list = await fs.readdirSync(backupsPath);
-  return list;
-  } catch (e)  {
-    throw new Error(e)
+    const backupsPath = path.join(__dirname, "../../../../backups");
+    let list = await fs.readdirSync(backupsPath);
+    return list;
+  } catch (e) {
+    throw new Error(e);
   }
-}
+};
