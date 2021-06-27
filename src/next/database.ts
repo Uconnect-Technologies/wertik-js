041f4784dcba340e0da04cd6b3543cb321f485ad
ilyas <ilyas.datoo@gmail.com>
2021-06-27 20:40:15 +0500

    Added relationship function

--- src/next/database.ts
+++ src/next/database.ts
@@ -1,4 +1,19 @@
 import { Sequelize } from "sequelize";
+
+export const getAllRelationships = () => {
+  return `
+    SELECT TABLE_NAME,
+      COLUMN_NAME,
+      REFERENCED_TABLE_NAME,
+      REFERENCED_COLUMN_NAME
+    FROM information_schema.KEY_COLUMN_USAGE
+    WHERE CONSTRAINT_SCHEMA = 'jscontainer'
+      AND REFERENCED_TABLE_SCHEMA IS NOT NULL
+      AND REFERENCED_TABLE_NAME IS NOT NULL
+      AND REFERENCED_COLUMN_NAME IS NOT NULL
+  `;
+};
+
 export const useDatabase = async function (obj: any) {
   const sequelize = new Sequelize(obj.name, obj.username, obj.password, {
     host: obj.host,
