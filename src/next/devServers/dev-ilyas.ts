65386e4ac855586da9a0671ada1ac5d82af6a7da
ilyas <ilyas.datoo@gmail.com>
2022-04-30 06:20:47 +0500

    Revert dev-ilyas file

--- src/next/devServers/dev-ilyas.ts
+++ src/next/devServers/dev-ilyas.ts
@@ -3,34 +3,33 @@ import { useGraphql } from "../graphql"
 import wertik, { useModule } from "../index"
 
 const devIlyas = async () => {
-  wertik(null)
-  // wertik({
-  //   port: 1200,
-  //   graphql: useGraphql(),
-  //   database: {
-  //     default: useMysqlDatabase({
-  //       port: 3306,
-  //       username: "root",
-  //       password: "pass",
-  //       name: "wertik",
-  //       host: "localhost",
-  //     }),
-  //   },
-  //   modules: {
-  //     forgetPassword: useModule({
-  //       name: "forgetPassword",
-  //       useDatabase: true,
-  //       database: "default",
-  //       table: "forgetPassword",
-  //     }),
-  //     shirts: useModule({
-  //       name: "shirts",
-  //       useDatabase: true,
-  //       database: "default",
-  //       table: "shirts",
-  //     }),
-  //   },
-  // })
+  wertik({
+    port: 1200,
+    graphql: useGraphql(),
+    database: {
+      default: useMysqlDatabase({
+        port: 3306,
+        username: "root",
+        password: "pass",
+        name: "wertik",
+        host: "localhost",
+      }),
+    },
+    modules: {
+      forgetPassword: useModule({
+        name: "forgetPassword",
+        useDatabase: true,
+        database: "default",
+        table: "forgetPassword",
+      }),
+      shirts: useModule({
+        name: "shirts",
+        useDatabase: true,
+        database: "default",
+        table: "shirts",
+      }),
+    },
+  })
 }
 
 export default devIlyas
