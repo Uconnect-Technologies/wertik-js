068db1601c55eeff7d4644134885994c614c900c
ilyas <ilyas.datoo@gmail.com>
2023-07-30 21:36:59 +0500

    Todo comment remove fix

--- test_database.sql
+++ test_database.sql
@@ -1,55 +1,29 @@
--- -------------------------------------------------------------
--- TablePlus 5.3.8(500)
---
--- https://tableplus.com/
---
--- Database: wertik
--- Generation Time: 2023-07-23 11:58:10.1740
--- -------------------------------------------------------------
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 -- Create database wertik if not exists
 CREATE DATABASE IF NOT EXISTS `wertik` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 
+USE `wertik`;
 
 DROP TABLE IF EXISTS `shirts`;
-CREATE TABLE `shirts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sizes` enum('lg','sm','xl','xxl','xxxl') DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
 DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(10) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
-INSERT INTO `shirts` (`id`, `sizes`, `user_id`) VALUES
-(1, 'xl', 1);
+CREATE TABLE `shirts` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `sizes` enum('lg','sm','xl','xxl','xxxl') DEFAULT NULL,
+  `user_id` bigint(20),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 INSERT INTO `users` (`id`, `name`, `email`) VALUES
 (12, '123', 'ads'),
 (13, '123', 'ads');
 
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+INSERT INTO `shirts` (`id`, `sizes`, `user_id`) VALUES
+(1, 'xl', 12);
